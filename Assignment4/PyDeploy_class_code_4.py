21d6fe3b040529a32752bfba8802fe45826cd8d7
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-04-08 16:53:13 +0330

    class_code_4

--- Assignment4/PyDeploy_class_code_4.py
+++ Assignment4/PyDeploy_class_code_4.py
@@ -20,3 +20,38 @@ def add_friends(id: str = Form(), name: str = Form(), age: float = Form()):
     friends[id]={"name": name, "age": age}
     return friends[id]
 
+@app.delete("/items/{id}")
+def remove(id: str):
+    if id not in friends:
+        raise HTTPException(status_code=404, detail= "Item not found!")
+    
+    del friends[id]
+    return{"message": "Item deleted!"}
+
+@app.put("/items/{id}")
+def update_friend(id: str, name: str= Form(None), age: float= Form(None)):
+    if id not in friends:
+        raise HTTPException(status_code=404, detail= "Item not found!")
+    
+    if name is not None:
+        friends[id]["name"]=name
+    if age is not None:
+        friends[id]["age"]=age
+    return friends[id]
+
+@app.post("/rgb2gray")
+async def rgb2gray(input_file: UploadFile = File(None)):
+    if not input_file.content_type.startswith("image/"):
+        raise HTTPException(status_code=415, detail= "Unsupported file type!")
+    
+    contents = await input_file.read()
+    np_array = np.frombuffer(contents, dtype=np.uint8)
+    img_rgb = cv2.imdecode(np_array. cv2.IMREAD_UNCHANGED)
+
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+
+    _, encoded_img = cv2.imencode(".png", img_gray)
+    img_bytes = encoded_img.tobytes()
+    return StreamingResponse(io.BytesIO(img_bytes), media_type="image/png")
+
+
