2d2568b6d19642a30a7f87838fd80db2ebe80b5c
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-04-09 16:02:42 +0330

    class_code_4

--- Assignment4/PyDeploy_class_code_4.py
+++ Assignment4/PyDeploy_class_code_4.py
@@ -46,12 +46,14 @@ async def rgb2gray(input_file: UploadFile = File(None)):
     
     contents = await input_file.read()
     np_array = np.frombuffer(contents, dtype=np.uint8)
-    img_rgb = cv2.imdecode(np_array. cv2.IMREAD_UNCHANGED)
+    img_rgb = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
+
+    # cv2.imwrite("test.jpg", img_rgb)
 
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 
-    _, encoded_img = cv2.imencode(".png", img_gray)
+    _, encoded_img = cv2.imencode(".jpg", img_gray)
     img_bytes = encoded_img.tobytes()
-    return StreamingResponse(io.BytesIO(img_bytes), media_type="image/png")
+    return StreamingResponse(io.BytesIO(img_bytes), media_type="image/jpeg")
 
 
