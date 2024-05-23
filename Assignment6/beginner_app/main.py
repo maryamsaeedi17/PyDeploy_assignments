1529f645a9028a4b4a2bc6136a13e6017cd817c0
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-05-23 09:21:34 +0330

    begginer-app

--- Assignment6/beginner_app/main.py
+++ Assignment6/beginner_app/main.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from fastapi import FastAPI
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi import FastAPI, HTTPException, Depends
+import sqlite3
+
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
@@ -23,4 +26,28 @@ class User(Base):
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    yield db
+    db.close()
+
+@app.get("/users")
+def read_user(user_id: int):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id==user_id).first()
+    db.close()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found!!")
+    return user
+
+@app.post("/users")
+def create_user(user_name: str, user_email: str):
+    db = SessionLocal()
+    user = User(name=user_name, email=user_email)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    db.close()
+    return user
 
