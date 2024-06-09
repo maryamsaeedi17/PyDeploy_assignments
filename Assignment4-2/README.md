253f61b23eb34017d5efec8bda53ef3ad4587401
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-06-09 21:54:41 +0330

    ass4-2-Flask

---
+++ Assignment4-2/README.md
@@ -0,0 +1,9 @@
+# PyDeploy_assignments
+The 12th assignment related to the PyDeploy course.
+
+## Flask
+
+## How to install :
+```
+pip install -r requirements.txt
+```
---
+++ Assignment4-2/app.py
@@ -0,0 +1,60 @@
+import os
+import cv2
+from flask import Flask, render_template, request, redirect, url_for, session
+
+
+app = Flask("Analyze Face")
+app.config["UPLOAD_FOLDER"] = './uploads'
+app.config["ALLOWED_EXTENSIONS"] = {'png', 'jpg', 'jpeg'}
+
+
+def auth(email, password):
+    if email == "maryam@maryam.saeedi" and password == "2468":
+        return True
+    else:
+        return False
+    
+def allowed_file(filename):
+    return True
+    
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        my_emil = request.form["email"]
+        my_password = request.form["password"]
+        result = auth(my_emil, my_password)
+        if result:
+            return redirect(url_for("upload"))
+        else:
+            # login
+            return redirect(url_for("login"))
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == "GET":
+        return(render_template("upload.html"))
+    elif request.method == "POST":
+
+
+        my_image = request.files['image']
+        if my_image.filename == "":
+            return redirect(url_for("upload"))
+        else:
+            if my_image and allowed_file(my_image.filename):
+                save_path = os.path.join(app.config["UPLOAD_FOLDER"], my_image.filename)
+                my_image.save(save_path)
+            return redirect(url_for("result"))
+
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
